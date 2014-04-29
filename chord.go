ec0d0aa8b8793b7cdf3928b5173c7fb6e6af5370
samanca <cse223amemarip@c08-11.sysnet.ucsd.edu>
2014-04-29 15:54:04 -0700

    Chord interface added

---
+++ chord.go
@@ -0,0 +1,13 @@
+package triblab
+import "trib"
+
+type Chord struct {
+
+}
+
+/**
+ * @return Returns a list of IP addresses that store the primary copy and replicas of the provided bin
+ */
+func (self *Chord) ReplicaSet(bin string) []string {
+	return nil
+}
