31d774d8a04b9680af4f121a7a519bb61030d6c8
Vineet Pandey <cse223vipandey@c08-11.sysnet.ucsd.edu>
2014-04-29 20:10:53 -0700

    iSome more changed to keeper.go * Still not complete, corner cases to fix for ring

--- chord.go
+++ chord.go
@@ -4,12 +4,20 @@ package triblab
 
 //comment by vineet
 type Chord struct {
-
+    ring []node
+    //check keeper.go for struct definitions
 }
 
 /**
  * @return Returns a list of IP addresses that store the primary copy and replicas of the provided bin
  */
-func (self *Chord) ReplicaSet(bin string) []string {
+func (self *Chord) ReplicaSetJoin(ip string) []string {
+    //ip is the IP address of the node which joined
+	return nil
+}
+
+func (self *Chord) ReplicaSetFail(ip string) []string {
+    //ip is the IP address of the successor node of the node which failed
+    //The failed node does not exist in the ring at all.
 	return nil
 }
--- keeper.go
+++ keeper.go
@@ -45,23 +45,24 @@ var ring []node
 
 //TODO-return ??
 //TODO- is the hash value returned uitn32?
-func addNodetoRing(ip string, ring []node){
+func addNodetoRing(ip string, ring []node, next *string, prev *string){
     var Node node
     Node.ip = ip
     val := getHash(ip)
     if (len(ring)==0){
         //TODO-Is this empty string?
-        Node.succ = make(string,0)
-        Node.prev = make (string,0)
+        Node.succ = ""
+        Node.prev = ""
         Node.start = 0
         Node.end = 2^(32) -1 //TODO-better way to write this
     } else{
-    for i:=0;i<len(Ring);i++{
+    for i:=0;i<len(ring);i++{
         if val > ring[i].start && val < ring[i].end{
             Node.succ = ring[i].succ
             Node.prev = ring[i].ip
             Node.start = ring[i].end
             Node.end = val
+            //Fix the predecessor
             ring[i].succ = Node.ip
             break
         }else {
@@ -69,7 +70,7 @@ func addNodetoRing(ip string, ring []node){
         }
     }
     //Fix the successor's arc and prev value
-    for j:=0;j<len(Ring);j++{
+    for j:=0;j<len(ring);j++{
         if ring[j].ip==Node.succ{
             ring[j].prev = Node.ip
             ring[j].start = val //TODO-or os this val + 1?
@@ -77,7 +78,33 @@ func addNodetoRing(ip string, ring []node){
         }
     }
 }
-    ring = append(Node) //TODO-is this how you use append
+    ring = append(ring, Node) //TODO-is this how you use append
+}
+
+
+func removeNodefromRing(ip string, ring []node, next *string, prev *string){
+    //assert(len(ring)!=0)
+
+    for i:=0;i<len(ring);i++{
+        if ip==ring[i].ip{
+            //we have found our node, need to modify the relevant succ and prev values in ring
+            //and remove the node
+
+            for j:=0;j<len(ring);j++{
+            //Fix the successor node
+                if ring[j].ip==ring[i].succ{
+                    ring[j].prev = ring [i].prev
+                }
+            //Fix the predecessor node
+                if ring[j].ip==ring[i].prev{
+                    ring[j].succ=ring[i].prev
+                }
+            }
+            //Remove the node
+            ring = append(ring[:i],ring[i+1:]...)
+            //TODO-AM i USING append correctly
+        }
+    }
 }
 
 //keeps a count mod 3. Everytime it is 0, we call the Clock().
@@ -126,6 +153,7 @@ func (self *keeper) run() error {
         if err_node_status!=nil{
             if(succ==true){
                 //the operation had succeeded, the node is up
+                //TODO-is my understanding of succ correct
                 cur_node_status=true
             }else{
                 //node is down
@@ -135,20 +163,27 @@ func (self *keeper) run() error {
             cur_node_status=true
         }
 
+        var succ2,succ3 bool
+        var next,prev string
+
         if node_status[i]==false{
             if cur_node_status==true{
             //new node has joined
             //TODO-modify ring
-            var next,prev string
-            addNodetoRing(self.config.Backs[i],&ring,&next,&prev)
+            addNodetoRing(self.config.Backs[i],ring,&next,&prev)
 
             //TODO-add successor/previous keys on the corresponding nodes
-            self.workers[i].handler.Set("NEXT",&next)
-            self.workers[i].Set("PREV",&prev)
+            self.workers[i].handler.Set(&KeyValue{
+                Key: "NEXT",
+                Value: next},&succ2)
+            self.workers[i].handler.Set(&KeyValue{
+                Key: "PREV",
+                Value: prev}, &succ3)
+                //TODO-Check for errors
+
 
             //TODO-call replication
-
-
+            //ReplicaSetJoin(self.config.Backs[i])
             }else{
                 //Nothing to do
             }
@@ -157,7 +192,14 @@ func (self *keeper) run() error {
                 //Nothing to do
             }else{
                 //Node has failed
-                //TODO-Modify ring, call replication
+                //TODO-Modify ring, remove node
+                removeNodefromRing(self.config.Backs[i],ring,&next,&prev)
+
+                //TODO-Modify successor/previous keys
+                //MODIFY THE CORRECT NODES
+
+                //TODO-call replication
+                //ReplicaSetFail(self.config.Backs[i])
             }
         }
     }
