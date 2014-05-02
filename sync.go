5f8a09f44bd462de572b9cb543b62dd683e0b1ca
samanca <cse223amemarip@c08-11.sysnet.ucsd.edu>
2014-05-02 12:34:39 -0700

    updated sync() - should work

--- sync.go
+++ sync.go
@@ -23,9 +23,12 @@ func _sync(backend string, replicas []string, chord *Chord) error {
 		rs[i] = &client{ addr: replicas[i] }
 	}
 
+	prev_ip, e := chord.Prev_node_ip(backend)
+	prev := &client{ addr: prev_ip }
+
 	// 1 - Retrieve OpLog
 	var opLog List
-	e := be.ListGet(LOG_KEY, &opLog)
+	e = be.ListGet(LOG_KEY, &opLog)
 	if e != nil { return e }
 
 	for i := range opLog.L {
@@ -35,36 +38,34 @@ func _sync(backend string, replicas []string, chord *Chord) error {
 		e = json.Unmarshal([]byte(opLog.L[i]), &op)
 		if e == nil {
 
-			// 2.5 - is it mine?
-			owner, err := chord.getIPbyBinName(removeNS(op.data.Key))
-
 			/*
 			 * if the current node is the owner, I just need to replicate data to the next and next_next node
 			 * otherwise, the previous node should be the owner, so I should write it to the previous and next node
 			 */
 
+			// 2.1 - I always have to write the data to the next node
+			e = _doWhatISay(rs[0], &op)
+			if e != nil { return e }
+
+			// 2.2 - is it mine?
+			owner, err := chord.getIPbyBinName(removeNS(op.data.Key))
 			if err != nil {
 				log.Print("error getting bin name: %s", err)
 			}
 
 			if owner == backend {
 
-				// 3 - Replicate
-				for r := range rs {
-					e = _doWhatISay(rs[r], &op)
+				// 2.3.1 - create the other replica
+				if (replicas[1] != backend) { // avoid storing data on the same node twice
+					e = _doWhatISay(rs[1], &op)
 					if e != nil { return e }
 				}
 
-				// 4 - Perform operation (we already have the data on the backend)
-				//e = _doWhatISay(be, &op)
-				//if e != nil { return e }
 			} else {
 
-				// 2.75 - Move it to the right place
-				var b bool
-				t := &client{ addr: owner }
-				e = t.ListAppend(&KeyValue{ Key: LOG_KEY, Value: opLog.L[i]}, &b)
-				// TODO optimizing this by maintaining a connection to previous node
+				// 2.3.2 - Write to the real owner
+				e = _doWhatISay(prev, &op)
+				if e != nil { return e }
 			}
 		}
 
