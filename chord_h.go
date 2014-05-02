6ebe9070413e36e1fda64ea17921fab7d6a3663d
Harsha <kharsha@eng.ucsd.edu>
2014-05-02 15:53:29 -0700

    Message

--- chord_h.go
+++ chord_h.go
@@ -10,11 +10,15 @@ type node1 struct {
     ip string //its own ip
     hash uint32
     start uint32 //start of its arc
-    end uint32 //end of its arc on ring
+    end uint32 //end of its arc on self.ring
 }
 
-//Initially there is no node1 in the ring
-var ring []node1
+
+type Chord1 struct {
+    back_ends[] string
+    ring []node1
+}
+
 
 func getHash1(name string) uint32 {
 	h := crc32.NewIEEE()
@@ -22,181 +26,165 @@ func getHash1(name string) uint32 {
 	return h.Sum32()
 }
 
-func (self *Chord1)locate_node(id uint32) uint32{
+func (self *Chord1) locate_node(id uint32) uint32{
     var succ uint32
     var id_max int
     var id_min int
     var max uint32
     var min uint32
     var r uint32
-//    var prev uint32
     found:=false
-    max=ring[0].hash//
-    min=ring[0].hash
+    max=self.ring[0].hash//
+    min=self.ring[0].hash
     r=0
-    fmt.Printf("Ring size is %d\n",len(ring))
-
-    if (len(ring)==1){
-            if(ring[0].hash>id){
-                succ=ring[0].hash
-                ring[0].end=id
-        //        prev=ring[0].hash
-          //      ring[0].begin=id
+    if (len(self.ring)==1){
+            if(self.ring[0].hash>id){
+                succ=self.ring[0].hash
+                self.ring[0].end=id
             }else{
-                succ= ring[0].end
-                ring[0].end=id
+                succ= self.ring[0].end
+                self.ring[0].end=id
             }
     }
-    if (len(ring)>=2){
+    if (len(self.ring)>=2){
 
-    for i:=0;i<len(ring);i++{
+    for i:=0;i<len(self.ring);i++{
 
-            if (ring[i].hash > max){
+            if (self.ring[i].hash > max){
                 id_max=i
-                max=ring[i].hash
+                max=self.ring[i].hash
             }
-            if (ring[i].hash<min){
+            if (self.ring[i].hash<min){
                 id_min=i
-                min=ring[i].hash
+                min=self.ring[i].hash
             }
 
 
-            if (id > ring[i].hash && id < ring[i].end){
-                succ=ring[i].end
-                ring[i].end=id
+            if (id > self.ring[i].hash && id < self.ring[i].end){
+                succ=self.ring[i].end
+                self.ring[i].end=id
                 found=true
                 break
             }
           
         }
         if (id >max || id <min) && found==false{
-                succ=ring[id_min].hash
-                ring[id_max].end=id
+                succ=self.ring[id_min].hash
+                self.ring[id_max].end=id
         }
 
-        }
-    
-    
-
-  /*          
-        if (id > ring[i].hash && id < ring[i].end && found==false) || (ring[i].hash==ring[i].end && found==false){
-            succ= ring[i].end
-            ring[i].end=id
-            found=true
-            r=1
-        }
-    }  
-        if (found=false && len(ring)>2) {
-            ring[id_max].end=id
-            succ=ring[id_min].hash
-            //succ=max
-            r=2
-        }
-
-        if (id < ring[id_min].hash && len(ring)>2) {
-             ring[id_max].end=id
-             succ=ring[id_min].hash
-             r=3
-        }
-    */   
-        //fmt.Printf("Max :%d,Max_id:%d,Min :%d,Min_id:%d,R:%d\n",ring[self.max_id].hash,self.max_id,ring[self.min_id].hash,self.min_id,r)
-        fmt.Printf("Max_id:%d,Min_id:%d,R:%d\n",max,min,r)              
+        }           
 return succ
 }
 
-func (self *Chord1)find_succ(id uint32) uint32{
+func (self *Chord1) find_succ(id uint32) uint32{
     var succ uint32
-    for i:=0;i<len(ring);i++{
-        if id > ring[i].hash && id < ring[i].end{
-            succ= ring[i].end
-            break
-        }else{
-        fmt.Printf("Didnt find the id on the circle")
+    var id_max int
+    var id_min int
+    var max uint32
+    var min uint32
+    var r uint32
+    found:=false
+    max=self.ring[0].hash
+    min=self.ring[0].hash
+
+    if len(self.ring)==1
+        return self.ring[0].hash
+
+    if len(self.ring)>1{
+        for i:=0;i<len(self.ring);i++{
+
+             if (self.ring[i].hash > max){
+                id_max=i
+                max=self.ring[i].hash
+            }
+
+            if (self.ring[i].hash<min){
+                id_min=i
+                min=self.ring[i].hash
+            }
+
+            if id > self.ring[i].hash && id < self.ring[i].end{
+                succ= self.ring[i].end
+                found=true
+                break
+            }
+        }
+           if (id >max || id <min) && found==false{
+                succ=self.ring[id_min].hash
         }
     }
 return succ
 }
 
-func (self *Chord1) createRing() node1{
-    var Node node1
-    Node.ip = ""
-    Node.hash=0
-    Node.end=4294967296 -1
-    return Node
-}
+
 func (self *Chord1) addNode(ip string){
     var Node node1
     Node.ip = ip
     val := getHash1(ip)
     Node.hash=val
-    if (len(ring)==0){
-    //self.createRing()
-    //ring = append(ring,self.createRing())
+    if (len(self.ring)==0){
     Node.end=val
     }else{
     Node.end=self.locate_node(val)
     }
-    ring = append(ring,Node)
-    for i:=0;i<len(ring);i++{
-    fmt.Printf("Node value:%d,Node Succ:%d\n",ring[i].hash,ring[i].end)
+    self.ring = append(self.ring,Node)
+    for i:=0;i<len(self.ring);i++{
+    fmt.Printf("Node value:%d,Node Succ:%d\n",self.ring[i].hash,self.ring[i].end)
     }
 }
-//TODO-r}eturn ??
-//TODO- is the hash value returned uitn32?
-func (self *Chord1) addNodetoRing(ip string){
-    var Node node1
-    Node.ip = ip
+
+
+func (self *Chord1) removeNode(ip string) (int,error){
+    var ip_used string
     val := getHash1(ip)
-    if (len(ring)==0){
-        //TODO-Is this empty string?
-        Node.succ = ""
-        Node.prev = ""
-        Node.start = 0
-        Node.end = 4294967296 -1 //TODO-better way to write this
-    }
-    for i:=0;i<len(ring);i++{
-        if val > ring[i].start && val < ring[i].end{
-            Node.succ = ring[i].succ
-            Node.prev = ring[i].ip
-            Node.start = ring[i].end
-            Node.end = val
-            ring[i].succ = Node.ip
-            break
-        }else {
-            fmt.Errorf("some error, the node must be inserted somewhere")
+    deleted := false
+    var index int
+    index=0
+    if (len(self.ring)==0){
+        fmt.Printf("No nodes to delete\n")
+    }else{     
+        if (len(self.ring)==1){
+            if self.ring[0].hash=val{
+            self.ring = make ([]node1, 0)
+            }
         }
+        deleted=true 
+    }else{
+        if (len(self.ring)==2){
+            if self.ring[0].hash==val{
+            ip_used=self.ring[1].ip
+            }else{
+            if self.ring[1].ip==ip{
+            ip_used=self.ring[0].ip
+            }
+            }
 
-    //Fix the successor's arc and prev value
-    for j:=0;j<len(ring);j++{
-        if ring[j].ip==Node.succ{
-            ring[j].prev = Node.ip
-            ring[j].start = val //TODO-or os this val + 1?
-            break
+        self.ring = make([]node,1)
+        self.ring[0].ip=ip_used
+        new_val := getHash1(ip_used)
+        self.ring[0].end= new_val
+        deleted=true
+        }
+    }else{
+        for i:=0;i<len(self.ring);i++{
+            if val==self.ring[i].hash{
+                self.ring = append(self.ring[:i],self.ring[i+1:]...)
+                deleted=true
+                index=i-1
+            }
         }
     }
-}
-    ring = append(ring,Node) //TODO-is this how you use append
+
+    if(deleted==false)
+        return fmt.Errorf("Error while deleting node in Chord")
+    else
+        return index,nil
+    
 }
 
-//keeps a count mod 3. Everytime it is 0, we call the Clock().
-//When it is 0,1, or 2, we do the node join/crash check
-var count1 int = -1
 
-//comment by vineet
-type Chord1 struct {
-	back_ends[] string
-    max_id int
-    min_id int
-}
-func (self *Chord1) makeRing(){
-for i := range self.back_ends{
-	self.addNode(self.back_ends[i])
-}
-}
 
-func (self *Chord1) printRing(){
-	for i := range ring {
-		//log.Printf("%s--%s--%s--%d--%d",ring[i].ip,ring[i].succ,ring[i].prev,ring[i].start,ring[i].end)
-        log.Printf("%s--%d--%d",ring[i].ip,ring[i].hash,ring[i].end)
-}
-}
+
+
+
