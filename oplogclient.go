7b83670e408f30d16b8ecb53e236548d26556c24
samanca <cse223amemarip@c08-11.sysnet.ucsd.edu>
2014-05-02 00:08:16 -0700

    @Harsha: bug fixes

--- oplogclient.go
+++ oplogclient.go
@@ -1,35 +1,20 @@
 package triblab
 import . "trib"
 import "net/rpc"
-//import "fmt"
-import "strings"
+import "encoding/json"
+import "fmt"
+//import "strings"
 
 type OpLogClient struct {
 	addr string
 	ns string
 }
 
-const (
-	LOG_KEY = "OPLOG"
-
-	OP_SET = 0 // opCode for Get()
-	OP_LIST_APPEND = 1 // opCode for ListAppend()
-	OP_LIST_REMOVE = 2 // opCode for ListRemove()
-)
-
-/*
- * Our OpLog will be a list of encoded OpLogEntries, ordered by Primary Time
- */
-type OpLogEntry struct {
-	opCode uint // OP_SET | OP_LIST_APPEND | OP_LIST_REMOVE
-	data KeyValue
-}
-
-func (self *client) acquireConnection() (*rpc.Client, error) {
+func (self *OpLogClient) acquireConnection() (*rpc.Client, error) {
 	return rpc.Dial("tcp", self.addr);
 }
 
-func (self *client) makeNS(key string) string {
+func (self *OpLogClient) makeNS(key string) string {
 	if (self.ns == "") {
 		return key
 	} else {
@@ -37,29 +22,11 @@ func (self *client) makeNS(key string) string {
 	}
 }
 
-func removeNS(entry string) string {
-	t := strings.Split(entry, "::")
-	if len(t) == 2 {
-		return t[1]
-	} else {
-		return t[0]
-	}
-}
-
-func extractNS(entry string) string {
-	t := strings.Split(entry, "::")
-	if len(t) == 2 {
-		return t[0]
-	} else {
-		return t[0] // EMPTY_STRING?!
-	}
-}
-
 /*
  * Implementing KeyString
  */
 
-func (self *client) Set(kv *KeyValue, succ *bool) error {
+func (self *OpLogClient) Set(kv *KeyValue, succ *bool) error {
 	c, err := self.acquireConnection();
 	if err == nil {
 		kv2 := KeyValue{ Key: self.makeNS(kv.Key), Value: kv.Value }
@@ -68,7 +35,7 @@ func (self *client) Set(kv *KeyValue, succ *bool) error {
 		if e != nil {
 		return fmt.Errorf("error while marshaling the OP Code")
 		}
-		OPkv := KeyValue{ Key:LOG_KEY , Value: op_j }
+		OPkv := KeyValue{ Key:LOG_KEY , Value: string(op_j) }
 		err = c.Call("Storage.ListAppend", &OPkv, succ);
 
 		c.Close()
@@ -77,7 +44,7 @@ func (self *client) Set(kv *KeyValue, succ *bool) error {
 }
 
 
-func (self *client) ListAppend(kv *KeyValue, succ *bool) error {
+func (self *OpLogClient) ListAppend(kv *KeyValue, succ *bool) error {
 	c, err := self.acquireConnection();
 	if err == nil {
 		kv2 := KeyValue{ Key: self.makeNS(kv.Key), Value: kv.Value }
@@ -86,14 +53,15 @@ func (self *client) ListAppend(kv *KeyValue, succ *bool) error {
 		if e != nil {
 		return fmt.Errorf("error while marshaling the OP Code")
 		}
-		OPkv := KeyValue{ Key:LOG_KEY , Value: op_j }
+		OPkv := KeyValue{ Key:LOG_KEY , Value: string(op_j) }
 		err = c.Call("Storage.ListAppend", &OPkv, succ);
 		c.Close()
 	}
 	return err;
 }
 
-func (self *client) ListRemove(kv *KeyValue, n *int) error {
+func (self *OpLogClient) ListRemove(kv *KeyValue, n *int) error {
+	var succ bool
 	c, err := self.acquireConnection();
 	if err == nil {
 		kv2 := KeyValue{ Key: self.makeNS(kv.Key), Value: kv.Value }
@@ -102,7 +70,7 @@ func (self *client) ListRemove(kv *KeyValue, n *int) error {
 		if e != nil {
 		return fmt.Errorf("error while marshaling the OP Code")
 		}
-		OPkv := KeyValue{ Key:LOG_KEY , Value: op_j }
+		OPkv := KeyValue{ Key:LOG_KEY , Value: string(op_j) }
 		err = c.Call("Storage.ListAppend", &OPkv, succ);
 		c.Close()
 	}
@@ -113,7 +81,7 @@ func (self *client) ListRemove(kv *KeyValue, n *int) error {
 /*
  * Implementing Storage
  */
-func (self *client) Clock(atLeast uint64, ret *uint64) error {
+func (self *OpLogClient) Clock(atLeast uint64, ret *uint64) error {
 	c, err := self.acquireConnection();
 	if err == nil {
 		err = c.Call("Storage.Clock", atLeast, ret);
