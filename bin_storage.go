3916121ed8b242d5d8b3b98ff0e91eacfe7d52aa
samanca <cse223amemarip@c08-11.sysnet.ucsd.edu>
2014-05-03 01:54:15 -0700

    simple failures work in client side

--- bin_storage.go
+++ bin_storage.go
@@ -1,4 +1,5 @@
 package triblab
+
 import . "trib"
 import "hash/crc32"
 import "time"
@@ -6,193 +7,232 @@ import "fmt"
 import "log"
 
 type BinStorageWrapper struct {
-	back_ends[] string
-	chord  Chord1
+    back_ends []string
+    chord     Chord1
 }
 
 func getHash(name string) uint32 {
-	h := crc32.NewIEEE()
-	h.Write([]byte(name))
-	return h.Sum32()
+    h := crc32.NewIEEE()
+    h.Write([]byte(name))
+    return h.Sum32()
 }
 
 func (self *BinStorageWrapper) Map(name string) uint32 {
-	h := getHash(name)
-	c := uint32(len(self.back_ends))
-	return h % c
+    h := getHash(name)
+    c := uint32(len(self.back_ends))
+    return h % c
 }
 
 func (self *BinStorageWrapper) Bin(name string) Storage {
-	var cli *client
-	var ip string
-	id:=self.chord.getHash1(name) // hash value for name
-	_,succ_ip:=self.chord.find_succ(id)
+
+	if LogEnabled { log.Print(self.chord.ring) }
+
+    var cli *client
+    var ip string
+
+	id := self.chord.getHash1(name) // hash value for name
+    _, succ_ip := self.chord.find_succ(id) // it should be the owner
+
 	if succ_ip == EMPTY_STRING {
-		log.Printf("succ_ip == EMPTY")
-		return nil
-	}
-	cli=&client{ addr: succ_ip }
-    _, err := cli.acquireConnection();
-    if(err == nil) {
-    	ip=succ_ip
-    } else {
-		log.Printf("trying to find another successor ...")
-    	for {
-            succ_ip_val := self.chord.getHash1(succ_ip)
-    		_, succ_ip := self.chord.find_succ(succ_ip_val)
-			cli = &client{ addr: succ_ip }
-    		_, err := cli.acquireConnection();
-
-    		if (err == nil) {
-    			ip = succ_ip
-    			break
-    		}
-    	}
+        log.Printf("succ_ip == EMPTY")
+        return nil
     }
-    
-	final_cli := &OpLogClient{ addr: ip, ns: name }
-	final_cli.init()
 
-	log.Printf("Connected to %s", ip)
+    cli = &client{addr: succ_ip}
+    c1, err := cli.acquireConnection() // test if the guy is up
+    if err == nil {
+        ip = succ_ip
+		c1.Close()
+    } else {
+		if LogEnabled { log.Printf("trying to find another successor ...") }
 
-	return final_cli
+		succ_ip_val := self.chord.getHash1(succ_ip) + 1
+		_, succ_ip := self.chord.find_succ(succ_ip_val) // the successor should be able to respond
+
+		if succ_ip == EMPTY_STRING{
+			log.Printf("succ_ip is empty string in Bin(%s)", name)
+		}
+
+		cli = &client{addr: succ_ip}
+		c2, err := cli.acquireConnection() // test if the guy is up
+
+		if err == nil {
+			ip = succ_ip
+			c2.Close()
+		} else {
+			log.Printf("DANGER: two consecutive nodes are down!!!!")
+			return nil
+		}
+    }
+
+    final_cli := &OpLogClient{addr: ip, ns: name}
+    final_cli.init()
+
+    log.Printf("Connected to %s", ip)
+
+    return final_cli
 }
 
 func (self *BinStorageWrapper) bootStrapRing() {
-	var cli *client
-	if LogEnabled { log.Print("Entered the ring") }
-    for i := 0; i < len(self.back_ends); i++ {
-    	cli = &client{ addr: self.back_ends[i] }
-    	_, err := cli.acquireConnection();
-    	if err == nil{
-    		self.chord.addNode(self.back_ends[i])
-    	}
+    var cli *client
+    if LogEnabled {
+        log.Print("Entered the ring")
     }
-	if LogEnabled {
-		log.Print("Exit the ring")
-		log.Print("Ring Size")
-		log.Print(len(self.chord.ring))
-	}
+    for i := 0; i < len(self.back_ends); i++ {
+        cli = &client{addr: self.back_ends[i]}
+        _, err := cli.acquireConnection()
+        if err == nil {
+            self.chord.addNode(self.back_ends[i])
+        }
+    }
+    if LogEnabled {
+        log.Print("Exit the ring")
+        log.Print("Ring Size")
+        log.Print(len(self.chord.ring))
+    }
 }
 
 func (self *BinStorageWrapper) fixPreviousPointer() {
-	var prev string
-	var prev_val uint32
+    var prev string
+    var prev_val uint32
     var cli *client
-	if LogEnabled {
-		log.Print("Entered the fixPrevious")
-	}
-	for i:= range self.chord.ring {
-		cli = &client{ addr: self.chord.ring[i].ip }
-    	_, err := cli.acquireConnection();
-    	if err == nil {
-    		err1 := cli.Get("PREV", &prev)
-            if (err1 != nil){
+    if LogEnabled {
+        log.Print("Entered the fixPrevious")
+    }
+    for i := range self.chord.ring {
+        cli = &client{addr: self.chord.ring[i].ip}
+        _, err := cli.acquireConnection()
+        if err == nil {
+            err1 := cli.Get("PREV", &prev)
+            if err1 != nil {
                 fmt.Errorf("Error with Get PREV")
                 //log.Print("Error while Get PREV")
             } else {
-				if LogEnabled { log.Print("PREV-", prev) }
-				prev_val=self.chord.getHash1(prev)
-				self.chord.ring[i].prev_ip=prev
-				self.chord.ring[i].prev=prev_val
-        	}
-    	}
-		if LogEnabled { log.Print("Exit fixPrevious") }
-	}
+                if LogEnabled {
+                    log.Print("PREV-", prev)
+                }
+                prev_val = self.chord.getHash1(prev)
+                self.chord.ring[i].prev_ip = prev
+                self.chord.ring[i].prev = prev_val
+            }
+        }
+        if LogEnabled {
+            log.Print("Exit fixPrevious")
+        }
+    }
 }
 
-func (self *BinStorageWrapper) updateRing(){
-	var cli *client
-	var next string
-	var prev string
-	var next_val uint32
-	var prev_val uint32
-//	var name string
+func (self *BinStorageWrapper) updateRing() {
+    var cli *client
+    var next string
+    var prev string
+    var next_val uint32
+    var prev_val uint32
+    //	var name string
     var incr uint32
-	var ip string
-    incr =0
+    var ip string
+    incr = 0
+    var found bool
+    status := 0
+    if LogEnabled {
+        log.Print("Entering updateRing")
+    }
 
-	if LogEnabled { log.Print("Entering updateRing") }
-
-	for {
-		// Run every 15 seconds
-		if LogEnabled {
-			log.Print("Running updateRing for:")
-			log.Print(incr)
-		}
+    for {
+        // Run every 15 seconds
+        if LogEnabled {
+            log.Print("Running updateRing for:")
+            log.Print(incr)
+        }
         incr = incr + 1
-		time.Sleep(15 * time.Second)
-		if LogEnabled { log.Print(len(self.chord.ring)) }
-		for i:= range self.chord.ring{
-			ip = self.chord.ring[i].ip
+        time.Sleep(15 * time.Second)
+        if LogEnabled {
+            log.Print(len(self.chord.ring))
+        }
 
-			cli = &client{ addr: ip }
+        for i := range self.chord.ring {
+            log.Print("Value of i and size of ring just before crashing - ", i, "and ", len(self.chord.ring))
+            self.printRing()
+            ip = self.chord.ring[i].ip
 
-			_, err := cli.acquireConnection();
-			if (err == nil){ // Node is alive
-				// Read PREV and NEXT from the live node
-				err1 := cli.Get("NEXT", &next)
-				err2 := cli.Get("PREV", &prev)
+            cli = &client{addr: ip}
 
-				if err1 != nil {
-					fmt.Errorf("Error with Get NEXT")
-					//log.Print("Error while getting Next")
-				}
-				if err2 != nil {
-					fmt.Errorf("Error with Get PREV")
-					//log.Print("Error while getting PREV")
-				}
+            _, err := cli.acquireConnection()
+            if err == nil { // Node is alive
+                // Read PREV and NEXT from the live node
+                err1 := cli.Get("NEXT", &next)
+                err2 := cli.Get("PREV", &prev)
 
-				next_val=self.chord.getHash1(next)
-				prev_val=self.chord.getHash1(prev)
+                if err1 != nil {
+                    fmt.Errorf("Error with Get NEXT")
+                    //log.Print("Error while getting Next")
+                }
+                if err2 != nil {
+                    fmt.Errorf("Error with Get PREV")
+                    //log.Print("Error while getting PREV")
+                }
 
-				if LogEnabled {
-					log.Print("Got next and pre values")
-					log.Print(self.chord.ring[i].next)
-					log.Print(next_val)
-				}
+                next_val = self.chord.getHash1(next)
+                prev_val = self.chord.getHash1(prev)
 
-				if (self.chord.ring[i].next!=next_val || self.chord.ring[i].prev!=prev_val){
-					// New node was added or some node was deleted
-					found:= false
-					for j:= range self.chord.ring {
-						if (self.chord.ring[j].ip == next){
-							found =true
-							break
-						}
-					}
-					if (found == true) {
-						self.chord.removeNode(next)// This should remove the node as well as the fix succ and prev
-					} else {
-						self.chord.addNode(next)
-					}
-				}
-			} else { // if the connection was not successful then remove that node
-				self.chord.removeNode(ip)
-			}
-		}
-	}
-	self.printRing()
-	if LogEnabled { log.Print("Leaving updateRing") }
+                if LogEnabled {
+                    log.Print("Got next and pre values")
+                    log.Print(self.chord.ring[i].next)
+                    log.Print(next_val)
+                }
+
+                if self.chord.ring[i].next != next_val || self.chord.ring[i].prev != prev_val {
+                    // New node was added or some node was deleted
+                    found = false
+                    for j := range self.chord.ring {
+                        if self.chord.ring[j].ip == next {
+                            found = true
+                            status = 1
+                            break
+                        }
+                    }
+
+                    if found == false {
+                        status = 2
+                    }
+                }
+            } else { // if the connection was not successful then remove that node
+                status = 3
+                //self.chord.removeNode(ip)
+            }
+        }
+
+        if status == 1 {
+            self.chord.removeNode(next) // This should remove the node as well as the fix succ and prev
+        } else if status == 2 {
+            self.chord.addNode(next)
+        } else if status == 3 { // if the connection was not successful then remove that node
+            self.chord.removeNode(ip)
+        }
+
+    }   //END OF update timer
+    self.printRing()
+    if LogEnabled {
+        log.Print("Leaving updateRing")
+    }
 }
 
 func (self *BinStorageWrapper) printRing() {
     log.Print("Print the ring")
     log.Print("Size of the ring PrintLog")
     log.Print(len(self.chord.ring))
-    for i:= range self.chord.ring {
-        fmt.Printf("%d--%d--%d--%s--%s\n",self.chord.ring[i].hash,self.chord.ring[i].prev,self.chord.ring[i].next,self.chord.ring[i].ip,self.chord.ring[i].succ_ip)
+    for i := range self.chord.ring {
+        fmt.Printf("%d--%d--%d--%s--%s\n", self.chord.ring[i].hash, self.chord.ring[i].prev, self.chord.ring[i].next, self.chord.ring[i].ip, self.chord.ring[i].succ_ip)
     }
     log.Print("Getting out of the Printring")
 }
 
 func (self *BinStorageWrapper) testFindSucc() {
-	ls:=[]uint32{562156532,653006829,674734111,825012062,947739488,949674805,1058053961,1073162028,1177263824,1209511639,1223694018,1333422798}
+    ls := []uint32{562156532, 653006829, 674734111, 825012062, 947739488, 949674805, 1058053961, 1073162028, 1177263824, 1209511639, 1223694018, 1333422798}
 
-
-	for i:= range ls{
-		succ,succ_ip:=self.chord.find_succ(ls[i])
-		log.Print("SUCC--",succ,"--",succ_ip)
-	}
+    for i := range ls {
+        succ, succ_ip := self.chord.find_succ(ls[i])
+        log.Print("SUCC--", succ, "--", succ_ip)
+    }
 
 }
--- chord_h.go
+++ chord_h.go
@@ -1,216 +1,222 @@
 package triblab
+
 //import "trib"
 import "hash/crc32"
 import "log"
 import "fmt"
+
 //Maintain node1 and Ring for the Chord1 Ring information
 type node1 struct {
     succ_ip string //succ ip
     prev_ip string //prev ip
-    ip string //its own ip
-    hash uint32
-    prev uint32 //prev of its arc
-    next uint32 //next of its arc on self.ring
+    ip      string //its own ip
+    hash    uint32
+    prev    uint32 //prev of its arc
+    next    uint32 //next of its arc on self.ring
 }
-
 
 type Chord1 struct {
     ring []node1
 }
 
-
-func (self *Chord1)getHash1(name string) uint32 {
-	h := crc32.NewIEEE()
-	h.Write([]byte(name))
-	return h.Sum32()
+func (self *Chord1) getHash1(name string) uint32 {
+    h := crc32.NewIEEE()
+    h.Write([]byte(name))
+    return h.Sum32()
 }
 
-func (self *Chord1) locate_node(id uint32,ip string) (ret_succ uint32,ret_succ_ip string){
+func (self *Chord1) locate_node(id uint32, ip string) (ret_succ uint32, ret_succ_ip string) {
     var succ uint32
-    var succ_ip string
+    var succ_ip string = EMPTY_STRING
     var id_max int
     var id_min int
     var max uint32 = 0
     var min uint32 = 4294967295
-    found:=false
+    found := false
     //max=self.ring[0].hash
     //min=self.ring[0].hash
     log.Print("Enter locate node")
-   // log.Print(self.ring)
-    if (len(self.ring)==1){
-            if(self.ring[0].hash>id){
-                succ=self.ring[0].hash
-                succ_ip=self.ring[0].ip
-                self.ring[0].succ_ip=ip
-                self.ring[0].next=id
+    log.Print(self.ring)
 
-            }else{
-                succ= self.ring[0].next
-                succ_ip=self.ring[0].succ_ip
-                self.ring[0].succ_ip=ip
-                self.ring[0].next=id
-            }
+    if len(self.ring) == 0 {
+        log.Printf("empty succ_ip, ring is zero nodes in locate_node")
+        return 0, EMPTY_STRING
     }
 
-    if (len(self.ring)>=2){
+    if len(self.ring) == 1 {
+        log.Printf("[1]")
+        if self.ring[0].hash > id {
+            succ = self.ring[0].hash
+            succ_ip = self.ring[0].ip
+            self.ring[0].succ_ip = ip
+            self.ring[0].next = id
 
-    for i:=0;i<len(self.ring);i++{
+        } else {
+            succ = self.ring[0].next
+            succ_ip = self.ring[0].succ_ip
+            self.ring[0].succ_ip = ip
+            self.ring[0].next = id
+        }
+    }
 
-            if (self.ring[i].hash > max){
-                id_max=i
-                max=self.ring[i].hash
+    if len(self.ring) >= 2 {
+        log.Printf("[2]")
+
+        for i := 0; i < len(self.ring); i++ {
+
+            if self.ring[i].hash > max {
+                id_max = i
+                max = self.ring[i].hash
             }
-            if (self.ring[i].hash<min){
-                id_min=i
-                min=self.ring[i].hash
+            if self.ring[i].hash < min {
+                id_min = i
+                min = self.ring[i].hash
             }
 
-
-            if (id > self.ring[i].hash && id < self.ring[i].next){
-                succ=self.ring[i].next
-                succ_ip=self.ring[i].succ_ip
-                self.ring[i].next=id
-                self.ring[i].succ_ip=ip
-                found=true
+            if id > self.ring[i].hash && id < self.ring[i].next {
+                log.Printf("[21]")
+                succ = self.ring[i].next
+                succ_ip = self.ring[i].succ_ip
+                self.ring[i].next = id
+                self.ring[i].succ_ip = ip
+                found = true
                 break
             }
-          
+
         }
-        if (id >max || id <min) && found==false{
-                succ=self.ring[id_min].hash
-                succ_ip=self.ring[id_min].ip
-                self.ring[id_max].next=id
-                self.ring[id_max].succ_ip=ip
+        if (id > max || id < min) && found == false {
+            log.Printf("[22]")
+            succ = self.ring[id_min].hash
+            succ_ip = self.ring[id_min].ip
+            self.ring[id_max].next = id
+            self.ring[id_max].succ_ip = ip
         }
 
-        }     
-        log.Print("Exit locate node")      
-return succ,succ_ip
+    }
+    log.Print("Exit locate node")
+    return succ, succ_ip
 }
 
-func (self *Chord1) find_succ(id uint32) (ret_succ uint32,ret_succ_ip string){
+func (self *Chord1) find_succ(id uint32) (ret_succ uint32, ret_succ_ip string) {
     var succ uint32
     var succ_ip string = EMPTY_STRING
     var id_min int
-    var max uint32
-    var min uint32
-    found:=false
-    //max=self.ring[0].hash
-    //min=self.ring[0].hash
-    max=0
-    min=4294967295
-	log.Printf("Ring size: %d", len(self.ring))
-	if len(self.ring) == 0 {
-		return 0, EMPTY_STRING
-	}
-	if len(self.ring)==1{
-        return self.ring[0].hash,self.ring[0].ip
-    }
-    if len(self.ring)>1{
-		log.Printf("Starting to do sth")
-        for i:=0;i<len(self.ring);i++{
+    var max uint32 = 0
+    var min uint32 = 4294967295
+    found := false
 
-             if (self.ring[i].hash > max){
-                max=self.ring[i].hash
+	if LogEnabled { log.Printf("Ring size: %d", len(self.ring)) }
+    if len(self.ring) == 0 {
+        log.Printf("IN ring of size 0. !!!! IN ind_succ")
+        return 0, EMPTY_STRING
+    }
+    if len(self.ring) == 1 {
+        return self.ring[0].hash, self.ring[0].ip
+    }
+    if len(self.ring) > 1 {
+        if LogEnabled { log.Printf("Starting to do sth") }
+        for i := 0; i < len(self.ring); i++ {
+			if LogEnabled {log.Printf("in find_succ - stuck not working") }
+            if self.ring[i].hash > max {
+                max = self.ring[i].hash
             }
 
-            if (self.ring[i].hash<min){
-                id_min=i
-                min=self.ring[i].hash
+            if self.ring[i].hash < min {
+                id_min = i
+                min = self.ring[i].hash
             }
 
             if id > self.ring[i].hash && id <= self.ring[i].next {
-				log.Printf("[1]")
-                succ= self.ring[i].next
-                succ_ip=self.ring[i].succ_ip
-                found=true
+                if LogEnabled { log.Printf("[1]") }
+                succ = self.ring[i].next
+                succ_ip = self.ring[i].succ_ip
+                found = true
                 break
             }
         }
 
-		if (id >max || id <min) && found==false{
-		   log.Printf("[2]")
-			succ=self.ring[id_min].hash
-			succ_ip=self.ring[id_min].ip
-			found=true
-		}
+        if (id > max || id < min) && found == false {
+			if LogEnabled { log.Printf("[2]") }
+            succ = self.ring[id_min].hash
+            succ_ip = self.ring[id_min].ip
+			if LogEnabled { log.Printf("succ_ip in find_succ is %s",succ_ip) }
+            found = true
+        }
     }
-return succ,succ_ip
+    return succ, succ_ip
 }
 
-
-func (self *Chord1) addNode(ip string){
+func (self *Chord1) addNode(ip string) {
     var Node node1
     Node.ip = ip
-    Node.succ_ip=""
-    Node.prev_ip=""
-    Node.prev=0
+    Node.succ_ip = ""
+    Node.prev_ip = ""
+    Node.prev = 0
     val := self.getHash1(ip)
-    Node.hash=val
-    if (len(self.ring)==0){
-    Node.next=val
-    Node.succ_ip=ip
-    }else{
-    Node.next,Node.succ_ip=self.locate_node(val,ip)
+    Node.hash = val
+    if len(self.ring) == 0 {
+        Node.next = val
+        Node.succ_ip = ip
+    } else {
+        Node.next, Node.succ_ip = self.locate_node(val, ip)
     }
-    self.ring = append(self.ring,Node)
-   
+    self.ring = append(self.ring, Node)
+
 }
 
-
-func (self *Chord1) removeNode(ip string) (error){
+func (self *Chord1) removeNode(ip string) error {
     var ip_used string
     val := self.getHash1(ip)
-    deleted:=false
-
-    if (len(self.ring)==0){
+    deleted := false
+    log.Print("Entering remove node")
+    if len(self.ring) == 0 {
         fmt.Printf("No nodes to delete\n")
-    }else if (len(self.ring)==1){
-            if self.ring[0].hash==val{
-            self.ring = make ([]node1, 0)
+    } else if len(self.ring) == 1 {
+        if self.ring[0].hash == val {
+            self.ring = make([]node1, 0)
+        }
+        deleted = true
+    } else if len(self.ring) == 2 {
+        if self.ring[0].hash == val {
+            ip_used = self.ring[1].ip
+        } else {
+            if self.ring[1].ip == ip {
+                ip_used = self.ring[0].ip
             }
-            deleted=true
-    }else if (len(self.ring)==2){
-            if self.ring[0].hash==val{
-            ip_used=self.ring[1].ip
-            }else{
-            if self.ring[1].ip==ip{
-            ip_used=self.ring[0].ip
-            }
-            }
+        }
 
-            self.ring = make([]node1,1)
-            self.ring[0].ip=ip_used
-            new_val := self.getHash1(ip_used)
-            self.ring[0].next= new_val
-            deleted=true
-    }else{
-        for i:=0;i<len(self.ring);i++{
-            if val==self.ring[i].hash{
-    
-                for j:=0;j<len(self.ring);j++{
-                    if self.ring[j].hash==self.ring[i].next{
+        self.ring = make([]node1, 1)
+        self.ring[0].ip = ip_used
+        self.ring[0].succ_ip = ip_used
+        new_val := self.getHash1(ip_used)
+        self.ring[0].hash = new_val
+        self.ring[0].next = new_val
+        deleted = true
+    } else {
+        for i := 0; i < len(self.ring); i++ {
+            if val == self.ring[i].hash {
+
+                for j := 0; j < len(self.ring); j++ {
+                    if self.ring[j].hash == self.ring[i].next {
                         self.ring[j].prev = self.ring[i].prev
                     }
-                if self.ring[j].hash==self.ring[i].prev{
-                    self.ring[j].next=self.ring[i].next
+                    if self.ring[j].hash == self.ring[i].prev {
+                        self.ring[j].next = self.ring[i].next
                     }
-            }
-                self.ring = append(self.ring[:i],self.ring[i+1:]...)
-                deleted=true
+                }
+                self.ring = append(self.ring[:i], self.ring[i+1:]...)
+                deleted = true
             }
         }
     }
 
-    if(deleted==false){
+    if deleted == false {
         return fmt.Errorf("Error while deleting node in Chord")
-    }else{
+    } else {
         return nil
-    }    
+    }
 }
 
 //func (self *Chord1) succ() {
 
 //}
-
-
